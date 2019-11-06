0a74bdcfc6cc986bcb7861852d29bfef4e62d51d
Adam Infiesto <adam.infiesto@us.ibm.com>
2019-11-06 10:52:11 -0600

    first

---
+++ src/android/DViewPlugin.java
@@ -0,0 +1,56 @@
+package cordova.plugin.voice;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class DViewPlugin extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("coolMethod")) {
+            String message = args.getString(0);
+            this.coolMethod(message, callbackContext);
+            return true;
+        }
+        if (action.equals("sorter")) {
+            //String message = args.getString(0);
+            this.sorter(args, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+    private void sorter(JSONArray args, CallbackContext callbackContext){
+        if(args != null){
+            try{
+                int passedData = Integer.parseInt(args.getJSONObject(0).getString("param1"));
+                
+                if(passedData == 1){
+                    callbackContext.success("One");
+                }
+                else{
+                    callbackContext.success("Some Other Number");
+                }
+            }catch(Exception e){
+                callbackContext.error("Your error" +e);
+            }
+        } else{
+            callbackContext.error("Null on Voice");
+        }
+    }
+}
